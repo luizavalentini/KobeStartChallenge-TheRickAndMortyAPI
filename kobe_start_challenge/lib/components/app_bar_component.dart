4a4538b7d304a2a17797023d67ec2a70558c813c
Luiza Valentini <luizadaravalentini@gmail.com>
2022-07-27 14:31:06 -0300

    feat(KobeStartChallenge): appBarComponent implemented

---
+++ kobe_start_challenge/lib/components/app_bar_component.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kobe_start_challenge/pages/home_page.dart';
+import 'package:kobe_start_challenge/theme/app_colors.dart';
+
+import '../theme/app_images.dart';
+
+PreferredSizeWidget appBarComponent(
+  BuildContext context, {
+  bool isSecodPage = false,
+}) {
+  return AppBar(
+    toolbarHeight: kToolbarHeight * 2.2,
+    backgroundColor: AppColors.appBarColor,
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    leading: Align(
+      alignment: Alignment.topCenter,
+      child: GestureDetector(
+        onTap: () {
+          final currentRoute = ModalRoute.of(context)?.settings.name;
+
+          if (currentRoute == HomePage.routeId) return;
+
+          Navigator.of(context).pop();
+        },
+        child: Icon(
+          Icons.menu,
+        ),
+      ),
+    ),
+    actions: [
+      Container(
+        alignment: Alignment.topCenter,
+        margin: const EdgeInsets.only(right: 16),
+        child: const Icon(
+          Icons.person,
+          color: AppColors.white,
+        ),
+      )
+    ],
+    flexibleSpace: SafeArea(
+      child: Column(
+        children: [
+          Image.asset(AppImages.logo),
+          const Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Text(
+                "RICKY AND MORTY API",
+                style: TextStyle(
+                  color: AppColors.white,
+                ),
+              ))
+        ],
+      ),
+    ),
+  );
+}
