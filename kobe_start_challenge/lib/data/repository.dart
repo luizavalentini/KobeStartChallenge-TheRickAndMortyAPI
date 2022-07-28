2e9e1f26e31b3911b4036bdc6ea0dafc45f3e40f
Luiza Valentini <luizadaravalentini@gmail.com>
2022-07-28 20:04:47 -0300

    feat(KobeStartChallenge): adding detailed character on the repository

--- kobe_start_challenge/lib/data/repository.dart
+++ kobe_start_challenge/lib/data/repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:kobe_start_challenge/models/paginated_character.dart';
+import 'package:kobe_start_challenge/models/detailed_character.dart';
 
 abstract class Repository {
   static final _dio = Dio(
@@ -8,10 +9,18 @@ abstract class Repository {
     ),
   );
 
-  static Future<PaginatedCharacter> getCharacter(int page) async {
+  static Future<PaginatedCharacter> getPage(int page) async {
     final response = await _dio.get('/character/?page=$page');
     print(response.data);
     final data = PaginatedCharacter.fromJson(response.data);
+
+    return data;
+  }
+
+  static Future<DetailedCharacter> getCharacter(int characterId) async {
+    final response = await _dio.get('/character/?page=$characterId');
+    print(response.data);
+    final data = DetailedCharacter.fromJson(response.data);
 
     return data;
   }
