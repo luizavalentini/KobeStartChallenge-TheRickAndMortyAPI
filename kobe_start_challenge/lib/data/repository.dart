34a015e9adee497e74391315a47840f03c8abe45
Luiza Valentini <luizadaravalentini@gmail.com>
2022-07-28 19:53:24 -0300

    feat(KobeStartChallenge): fixing repository

--- kobe_start_challenge/lib/data/repository.dart
+++ kobe_start_challenge/lib/data/repository.dart
@@ -4,13 +4,13 @@ import 'package:kobe_start_challenge/models/paginated_character.dart';
 abstract class Repository {
   static final _dio = Dio(
     BaseOptions(
-        baseUrl: 'https://rickandmortyapi.com/api',
-       ),
-       );
+      baseUrl: 'https://rickandmortyapi.com/api',
+    ),
+  );
 
-  static Future<PaginatedCharacter> getCharacter() async {
-    final response = await _dio.get('/character');
-
+  static Future<PaginatedCharacter> getCharacter(int page) async {
+    final response = await _dio.get('/character/?page=$page');
+    print(response.data);
     final data = PaginatedCharacter.fromJson(response.data);
 
     return data;
