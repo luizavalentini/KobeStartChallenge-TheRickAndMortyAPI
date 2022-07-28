c4e092765950f458b4769a59e6ec5f3119080702
Luiza Valentini <luizadaravalentini@gmail.com>
2022-07-28 19:51:55 -0300

    feat(KobeStartChallenge): updating homePage

--- kobe_start_challenge/lib/pages/home_page.dart
+++ kobe_start_challenge/lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kobe_start_challenge/components/app_bar_component.dart';
+import 'package:kobe_start_challenge/components/character_card_component.dart';
 import 'package:kobe_start_challenge/data/repository.dart';
 import 'package:kobe_start_challenge/models/paginated_character.dart';
+import 'package:kobe_start_challenge/pages/details_page.dart';
 import 'package:kobe_start_challenge/theme/app_colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,7 +19,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    character = Repository.getPage();
+    character = Repository.getCharacter(0);
     super.initState();
   }
 
@@ -29,10 +31,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBarComponent(context),
-        backgroundColor: AppColors.backgroundColor,
-        body: FutureBuilder(builder: (_, snapshot) {
-          return Container();
-        }));
+      appBar: appBarComponent(context),
+      backgroundColor: AppColors.backgroundColor,
+      body: FutureBuilder(
+          future: character,
+          builder: (_, AsyncSnapshot<PaginatedCharacter> snapshot) {
+            if (snapshot.hasData) {
+              final dataResults = snapshot.data!.results;
+
+              return ListView.separated(
+                itemBuilder: (_, index) {
+                  final character = dataResults[index];
+
+                  return CharacterCard(
+                    character: character,
+                    onTap: () => Navigator.of(context).pushNamed(
+                      DetailsPage.routeId,
+                      arguments: character.id,
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 16),
+                itemCount: dataResults.length,
+              );
+            }
+            return Container();
+          }),
+    );
   }
 }
