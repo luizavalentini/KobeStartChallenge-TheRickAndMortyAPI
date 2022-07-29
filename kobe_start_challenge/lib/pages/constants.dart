57b6f41af73076234f9b9270f4008bffecba06ea
Luiza Valentini <luizadaravalentini@gmail.com>
2022-07-29 03:04:26 -0300

    feat(KobeStartChallenge): implemented constants for the details page

---
+++ kobe_start_challenge/lib/pages/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+CircleAvatar alive = CircleAvatar(
+  radius: 4,
+  backgroundColor: Colors.green,
+);
+CircleAvatar dead = CircleAvatar(
+  radius: 4,
+  backgroundColor: Colors.red,
+);
